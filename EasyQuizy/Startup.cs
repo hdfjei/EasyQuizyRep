dfd20a08372dad28fbbb1f20e430e2b13d1043e9
alkhovik <maks.alkhovik@gmail.com>
2017-05-31 13:34:31 +0300

    Добавлена аутентицикация

---
+++ EasyQuizy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Authentification.Startup))]
+namespace Authentification
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
